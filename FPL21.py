d29a09e2c4970ee9b5a11eb6b46d9460c1c7569d
yolobczuk <wsobczuk2@gmail.com>
2021-08-14 18:39:02 +0200

    Added new features

--- FPL21.py
+++ FPL21.py
@@ -19,10 +19,10 @@ def init_db():
     cur.execute('CREATE TABLE IF NOT EXISTS fixtures(gw INTEGER, id INTEGER, home_team INTEGER, away_team INTEGER, home_diff INTEGER, away_diff INTEGER, CONSTRAINT PK_fix PRIMARY KEY (gw, home_team, away_team), CONSTRAINT FK_team_fix1 FOREIGN KEY (home_team) REFERENCES teams(id), CONSTRAINT FK_team_fix2 FOREIGN KEY (away_team) REFERENCES teams(id))')
     cur.execute('CREATE TABLE IF NOT EXISTS players(id INTEGER PRIMARY KEY, web_name VARCHAR, web_name VARCHAR, team_id INTEGER, pos_id INTEGER)')
     cur.execute('CREATE TABLE IF NOT EXISTS picks(gw INTEGER, ent_id INTEGER, el_id INTEGER, mult INTEGER, points INTEGER, CONSTRAINT PK_picks PRIMARY KEY (gw, ent_id, el_id), CONSTRAINT FK_man_pick FOREIGN KEY (ent_id) REFERENCES managers(id), CONSTRAINT FK_play_pick FOREIGN KEY (el_id) REFERENCES players(id))')
-    cur.execute('CREATE TABLE IF NOT EXISTS stat(gw INTEGER, id INTEGER, every INTEGER, transfers INTEGER, capt INTEGER, vcapt INTEGER, bench INTEGER, value FLOAT, place INTEGER, CONSTRAINT PK_stat PRIMARY KEY(gw, id), CONSTRAINT FK_man_stat FOREIGN KEY (id) REFERENCES managers(id))')
+    cur.execute('CREATE TABLE IF NOT EXISTS stat(name VARCHAR, club VARCHAR, gw INTEGER, total INTEGER, transfers INTEGER, capt INTEGER, bench INTEGER, value FLOAT, CONSTRAINT PK_stat PRIMARY KEY(gw, name, club))')
     cur.execute('CREATE TABLE IF NOT EXISTS transfers(gw INTEGER, id INTEGER, transfers_in VARCHAR, transfers_out VARCHAR, CONSTRAINT PK_trans PRIMARY KEY(gw, id), CONSTRAINT FK_man_trans FOREIGN KEY (id) REFERENCES managers(id))')
     cur.execute('CREATE TABLE IF NOT EXISTS attributes(gw INTEGER, id INTEGER, event_points INTEGER, ict FLOAT, form FLOAT, value FLOAT, xPTS FLOAT, against VARCHAR, CONSTRAINT PK_att PRIMARY KEY (gw, id), CONSTRAINT FK_play_attr FOREIGN KEY (id) REFERENCES players(id))')
-        
+    cur.execute('CREATE TABLE IF NOT EXISTS transfer_history(gw INTEGER, id INTEGER, cost INTEGER, CONSTRAINT PK_trhis PRIMARY KEY (gw, id), CONSTRAINT FK_play_trhist FOREIGN KEY (id) REFERENCES managers(id))')
     con.commit()
 
 def init_managers():
@@ -132,7 +132,7 @@ def refresh_attributes(GW):
     temp = pd.DataFrame(json['elements'])
     
     attr = pd.DataFrame()
-    attr = temp[['id','ep_this','ict_index','form','now_cost','ep_next']]
+    attr = temp[['id','event_points','ict_index','form','now_cost','ep_next']]
     
     players = pd.read_sql("SELECT * FROM players", con)
     play_attr = pd.merge(players, attr, left_on = 'id', right_on = 'id')
@@ -140,13 +140,13 @@ def refresh_attributes(GW):
     fixtures = pd.read_sql('SELECT * FROM fixtures WHERE event = '+ str(GW), con)
     
     play_attr_fix_h = pd.merge(play_attr, fixtures, left_on = 'team', right_on = 'team_h')
-    play_attr_fix_h = play_attr_fix_h[['event','id','ep_this','ict_index','form','now_cost','ep_next','team_a']]
+    play_attr_fix_h = play_attr_fix_h[['event','id','event_points','ict_index','form','now_cost','ep_next','team_a']]
     play_attr_fix_h = play_attr_fix_h.rename(columns = {'team_a': 'against'})
     play_attr_fix_a = pd.merge(play_attr, fixtures, left_on = 'team', right_on = 'team_a')
-    play_attr_fix_a = play_attr_fix_a[['event','id','ep_this','ict_index','form','now_cost','ep_next','team_h']]
+    play_attr_fix_a = play_attr_fix_a[['event','id','event_points','ict_index','form','now_cost','ep_next','team_h']]
     play_attr_fix_a = play_attr_fix_a.rename(columns = {'team_h': 'against'})
     
-    attr_ag = play_attr_fix_h.append(play_attr_fix_a).sort_values(by = 'id').rename(columns = {'event': 'gw', 'ep_this': 'event_points', 'ict_index':'ict','ep_next':'xPTS','now_cost':'value'})
+    attr_ag = play_attr_fix_h.append(play_attr_fix_a).sort_values(by = 'id').rename(columns = {'event': 'gw', 'ict_index':'ict','ep_next':'xPTS','now_cost':'value'})
     attr_ag = attr_ag.fillna(value=0)
     
     attr_ag['value'] = attr_ag['value']/10
@@ -162,6 +162,7 @@ def init_picks(GW):
     refresh_attributes(GW)
     attributes = pd.read_sql("SELECT * FROM attributes", con = con)
     managers = pd.read_sql("SELECT * FROM managers", con = con)
+    transfers = pd.DataFrame()
     try:
         cur.execute("DELETE FROM picks WHERE gw = " + str(GW))
     except Exception as err:
@@ -178,10 +179,16 @@ def init_picks(GW):
         picks['ent_id'] = i
         picks = picks[['gw','ent_id','element','multiplier','event_points']]
         picks.columns = ['gw','ent_id','el_id','mult','points']
+        transfers = transfers.append([[1, i, json['entry_history']['event_transfers_cost']]])
         try:
             picks.to_sql('picks', con=con, if_exists='append', index=False)
         except Exception as err:
             print('Query failed: %s; continuing' % (str(err)))
+    transfers.columns = ['gw', 'id', 'cost']
+    try:
+        transfers.to_sql('transfer_history', con=con, if_exists='append', index=False)
+    except Exception as err:
+        print('Query failed: %s; continuing' % (str(err)))
             
 def init_player_info(GW):
     init_picks(GW)
@@ -209,6 +216,57 @@ def write_summary(GW):
     captain = pd.merge(captain, summary, left_on = 'name', right_on = 'name')
     captain = captain.rename(columns = {'web_name':'captain'})
     print(captain)
+    
+    
+def init_stats(ver = 'season', GW):
+    if(ver == 'season'):
+        info = pd.read_sql("SELECT * FROM player_info", con = con)
+    else:
+        info = pd.read_sql("SELECT * FROM player_info WHERE gw = " + str(GW), con = con)
+    positions = pd.read_sql("SELECT * FROM positions", con = con).set_index('id')
+    teams = pd.read_sql("SELECT * FROM teams", con = con).set_index('id')
+    
+    info['against'] = info.against.map(teams.name)
+    info['team'] = info.team.map(teams.name)
+    info['element_type'] = info.element_type.map(positions.name)
+    info['mult_points'] = info['mult']*info['points']
+    
+    fs = pd.pivot_table(info[info['mult']>0], values='mult_points', index=['name','club'], aggfunc=np.sum).sort_values(by='mult_points', ascending=False)
+    b = pd.pivot_table(info[info['mult']==0], values='points', index=['name','club'], aggfunc=np.sum).sort_values(by='points', ascending=False)
+    c = pd.pivot_table(info[info['mult']>=2], values='mult_points', index=['name','club'], aggfunc=np.sum).sort_values(by='mult_points', ascending=False)
+    v = pd.pivot_table(info, values='value', index=['name','club'], aggfunc=np.sum).sort_values(by='value', ascending=False)
+    t = pd.read_sql("SELECT t.gw, m.name, m.club, t.cost FROM managers m INNER JOIN transfer_history t ON t.id = m.id",con=con).set_index(['name','club'])
+    t = t.cost.cumsum()
+    
+    summ = pd.concat([fs, b, c, v, t], axis=1)
+    summ.columns = ['total','bench','captain','value','transfers']
+    summ['total'] = summ['total'] - summ['transfers']
+    summ = summ.sort_values(by = 'total',ascending = False)
+    summ['next'] = abs(summ['total'].diff()).fillna(0)
+    summ['leader'] = abs(summ['total'] - summ['total'].iloc[0])
+    if(ver == 'gw'):
+        summ['gw'] = GW
+        summ = summ[['gw','total','next','leader','transfers','captain','bench','value']]
+        try:
+            cur.execute("DELETE FROM stat WHERE gw = " + str(GW))
+            summ.to_sql('stat', con=con, if_exists='append', index=False)
+            summ.to_csv('stat_gw.csv', mode='a', index=False, encoding='utf-8-sig')
+        except Exception as err:
+            print('Query failed: %s; continuing' % (str(err)))
+    else:
+        summ = summ[['total','next','leader','transfers','captain','bench','value']]
+        summ.to_csv('stat_season.csv', index=False, encoding='utf-8-sig')
+        
+def export_picks():
+    info = pd.read_sql("SELECT * FROM player_info", con = con)
+    info['against'] = info.against.map(teams.name)
+    info['team'] = info.team.map(teams.name)
+    info['element_type'] = info.element_type.map(positions.name)
+    info['mult_points'] = info['mult']*info['points']
+    info[info['mult']>0].to_csv('data.csv', index=False, encoding='utf-8-sig')
+    
+def print_means():
+
     
 OK = True
 
